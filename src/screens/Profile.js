35aaf2413a7e85fb45d8c0904c77a31a9ebae18a
Siham <siham.hadi@live.se>
2020-04-27 11:45:22 +0200

    Background change

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -76,6 +76,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-evenly',
     alignItems: 'center',
     color: Colors.black,
+    backgroundColor: Colors.white,
     fontWeight: '300',
     height: 25,
     width: 100,
