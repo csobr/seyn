04fbc2f969a4fe1cf87a5289f0623d11a7cdb479
Siham <siham.hadi@live.se>
2020-05-01 12:10:33 +0200

    Changes to file structure - Moved App navigation. - Moved files - User Context WIP

--- src/components/User/UserContext.js
+++ src/components/User/UserContext.js
@@ -1,19 +1,12 @@
-import React, {useState, useContext, Fragment, createContext} from 'react';
-import {Text} from 'react-native';
+import React, {useState, useContext, createContext} from 'react';
 import UserApi from './UsersHook';
 
 const CurrentUser = createContext(null);
 
-const UserInfo = ({id, name, title, image, transport}) => {
+const UserInfo = () => {
   const user = useContext(CurrentUser);
   const {users} = UserApi();
   const [u, setUser] = useState(users);
-  return (
-    <React.Fragment>
-      {u.map(uid => (
-        <Text key={uid.id} />
-      ))}
-    </React.Fragment>
-  );
+  return <>{u.map(uid => uid.id)}</>;
 };
 export default UserInfo;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,7 +12,7 @@ import {useNavigation} from '@react-navigation/native';
 import Colors from '../constants/Colors';
 import GlobalStyle from '../GlobalStyles';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import UserApi from '../components/UsersHook';
+import UserApi from '../components/User/UsersHook';
 
 const Home = ({navigation: {navigate}}) => {
   const [{users}] = UserApi();
