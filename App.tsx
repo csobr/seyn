59b5d5bbe8683c5e0fcdfde735f0dbff6ab94032
csobr <siham.hadi@live.se>
2020-05-26 23:08:22 +0200

    reducer wip

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
 import {CurrentUser} from './src/components/User/UserContext';
+import Users from './src/components/User/UsersHook';
 import AppNavigation from './src/components/Navigation/AppNavigation';
 
+const usersReducer = (state, action) => {
+  switch (action.type) {
+    case 'SHOW_ALL':
+      return 'ALL';
+    case 'SHOW_ACTIVE':
+      return 'ACTIVE';
+    case 'SHOW_OFFLINE':
+      return 'OFFLINE';
+    default:
+      throw Error();
+  }
+};
 const App: React.FC = ({}) => {
+  const [{users}] = Users();
+  // const [filter, dispatchFilter] = React.useReducer(usersReducer, 'ALL');
+
   return (
     <>
-      <CurrentUser.Provider value={{name: 'Annie'}}>
+      <CurrentUser.Provider value={users}>
         <AppNavigation />
       </CurrentUser.Provider>
     </>
--- src/components/User/UserContext.tsx
+++ src/components/User/UserContext.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-type UserContextProps = {
-  name: string;
-};
+type UserContextProps = {};
 
 export const CurrentUser = React.createContext<Partial<UserContextProps>>({});
