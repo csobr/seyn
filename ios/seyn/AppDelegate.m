37136989b29f4408dd2a4ca8eff715f7bc1aff4e
csobr <siham.hadi@live.se>
2020-06-09 17:31:48 +0200

    android firebase error

--- ios/seyn/AppDelegate.m
+++ ios/seyn/AppDelegate.m
@@ -17,7 +17,6 @@
     if ([FIRApp defaultApp] == nil) {
       [FIRApp configure];
     }
-
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"seyn"
