745bf37afb503f105b33b6eb445991e8dce3d614
csobr <siham.hadi@live.se>
2020-08-11 10:04:36 +0200

    replaced icons to svg

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,15 +1,14 @@
 import react from 'react';
 import {Image} from 'react-native';
-import car from './bus.png';
 
 type name = {};
 const name = {
-  car: car,
+  car: '',
   bus: '',
   scooter: '',
   bike: '',
   train: '',
   taxi: '',
 };
-const Icon = () => <Image source={require(car)} />;
+const Icon = () => {};
 export default Icon;
