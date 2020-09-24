19c4498560578546f50a270edae77b609166ca92
csobr <siham.hadi@live.se>
2020-09-24 13:25:02 +0200

    edit message

--- src/components/Form/CustomErrorMessages.tsx
+++ src/components/Form/CustomErrorMessages.tsx
@@ -1,4 +1,5 @@
-export const USER_NOT_FOUND = 'Account does not exist.';
+export const USER_NOT_FOUND =
+  'The is no account with the email and password you entered. Please double-check and try again.';
 export const EMAIL_ALREADY_EXISTS = 'Email already in use.';
 export const INVALID_EMAIL = 'Email Invalid';
 export const WRONG_PASSWORD = 'Wrong password';
